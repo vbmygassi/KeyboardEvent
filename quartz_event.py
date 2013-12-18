7810f54bcfd77ae4d009294eb0641503df8ac300
viktorberzsinszky <vberzsin@gmail.com>
2013-12-18 12:10:09 +0100

    init

---
+++ quartz_event.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+#coding=utf-8
+
+'''
+http://stackoverflow.com/questions/1817628/clicking-the-mouse-down-to-drag-objects-on-mac
+http://stackoverflow.com/questions/18027342/how-can-i-call-cgeventkeyboardsetunicodestring-from-python
+'''
+
+import sys, time
+from Quartz.CoreGraphics import *
+
+def postMClickDownE(x, y):
+	e = CGEventCreateMouseEvent(None, kCGEventLeftMouseDown, (x, y), kCGMouseButtonLeft);
+	CGEventPost(kCGHIDEventTap, e);
+
+def postMClickUpE(x, y):
+	e = CGEventCreateMouseEvent(None, kCGEventLeftMouseUp, (x, y), kCGMouseButtonLeft);
+	CGEventPost(kCGHIDEventTap, e);
+
+def postMDragggedE(x, y):
+	e = CGEventCreateMouseEvent(None, kCGEventLeftMouseDragged, (x, y), kCGMouseButtonLeft);
+	CGEventPost(kCGHIDEventTap, e);
+
+def postMMoved(x, y):
+	e = CGEventCreateMouseEvent(None, kCGEventMouseMoved, (x, y), kCGMouseButtonLeft);
+	CGEventPost(kCGHIDEventTap, e);
+ 	
+def postKeyE(key):
+	e = CGEventCreateKeyboardEvent(None, 0, True);
+	CGEventKeyboardSetUnicodeString(e, len(key), map(ord, key));
+	CGEventPost(kCGHIDEventTap, e);
+
+# stores mouse position	
+e = CGEventCreate(None);
+pos = CGEventGetLocation(e);
+
+postMClickDownE(10, 10);
+postMDragggedE(20, 20);
+postMClickUpE(10, 10);
+
+# resets mouse position
+postMMoved(int(pos.x), int(pos.y));
+postMClickDownE(int(pos.x), int(pos.y));
+
+postKeyE(u'f');
+postKeyE(u'u');
+postKeyE(u'k');
+postKeyE(u'c');
+postKeyE(u'ü');
+
